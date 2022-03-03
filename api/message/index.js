1241a77f384ded8349bbc2b4555656156108b051
SupremeDPS <96889203+SupremeDPS@users.noreply.github.com>
2022-03-02 19:18:39 -0800

    Update index.js
    day added

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.res.json({
-        text: "Hello from this content.res"
+        text: "20220302 - Better for sorting I am thinking. But who really knows. Most likely no one. For now though, we will keep picking away at it. It actually doesn't take that much."
     });
 }
